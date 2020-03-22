bb898b38761dac0532c760a236bc656cd1b3520c
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-22 15:28:17 -0700

    HTML routes work

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
 const PORT = process.env.PORT || 3000;
 const app = express();
+// const api = require(`./routes/api-routes`);
+const html = require(`./routes/html-routes`);
 
 app.use(logger(`dev`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
-
+// api(app);
+html(app);
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/populatedb`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
