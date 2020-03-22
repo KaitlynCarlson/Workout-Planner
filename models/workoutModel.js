a50100ba878e119b9dea3248fe1015e94f2a7fa9
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-22 15:14:05 -0700

    Fleshing out workoutModel

---
+++ models/workoutModel.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const mongoose = require(`mongoose`);
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({});
+
+module.exports = mongoose.model(`Workout`, WorkoutSchema);
