90229c669a7c73618c9ec005ecc113a8794300c5
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-22 15:32:14 -0700

    Removed routes

--- server.js
+++ server.js
@@ -5,21 +5,30 @@ const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const api = require(`./routes/api-routes`);
-const html = require(`./routes/html-routes`);
 
 app.use(logger(`dev`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
-// api(app);
-html(app);
+
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/populatedb`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
+});
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/index.html`));
+});
+
+app.get(`/exercise`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/exercise.html`));
+});
+
+app.get(`/stats`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/stats.html`));
 });
