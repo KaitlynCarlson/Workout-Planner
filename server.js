bf12ba0dfd906f3580dc6b9b1146aa4601cec391
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-27 13:15:29 -0700

    Deploy

--- server.js
+++ server.js
@@ -11,18 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
 
-mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://localhost/mongoHeadlines`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useMongoClient: true
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 require(`./routes/api-Routes`)(app);
 require(`./routes/html-routes`)(app);
+
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
