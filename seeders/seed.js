68e83006424e4123b7b519b44f6927f962981aea
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-27 16:00:42 -0700

    Seed change

--- seeders/seed.js
+++ seeders/seed.js
@@ -5,7 +5,8 @@ const db = require(`../models`);
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useMongoClient: true
 });
 
 const workoutSeed = [
